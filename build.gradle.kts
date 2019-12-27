7d4a49fc6cf28291839df9bb33eeb28c7df6bc9a
hojong_home <hojong.jjh@gmail.com>
2019-12-28 03:32:07 +0900

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.61"
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+    }
+
+    group = "com.hojongs"
+    version = "1.0-SNAPSHOT"
+}
+
+subprojects {
+    apply {
+        plugin("kotlin")
+    }
+
+    dependencies {
+        implementation(kotlin("reflect"))
+        implementation(kotlin("stdlib-jdk8"))
+    }
+
+    java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "1.8"
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "todo-server"
+include(":todo-presentation")
